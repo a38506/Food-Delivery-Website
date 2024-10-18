28b7e1f42e1650ca59558cb97fce7b3646b24c58
a38506 <nbaoviet2@gmail.com>
2024-10-18 21:32:05 +0700

    complete

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { listOrders, placeOrder, userOrders, verifyOrder,updateStatus } from "../controllers/orderController.js";
+import authMiddleware from '../middleware/auth.js';
+
+const orderRoute = express.Router()
+
+orderRoute.post('/place', authMiddleware,placeOrder)
+orderRoute.post('/verify',verifyOrder)
+orderRoute.post('/userorders', authMiddleware,userOrders)
+orderRoute.get('/list',listOrders)
+orderRoute.post('/status',updateStatus)
+
+export default orderRoute
